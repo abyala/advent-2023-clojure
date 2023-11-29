1e18151b704dee22baea70b45c3347d9f41a38d5
Andrew Byala <abyala@yahoo.com>
2023-11-28 20:54:05 -0600

    First commit with deps.edn depending on my new util repo.

---
+++ src/advent_2023_clojure/day01.clj
@@ -0,0 +1,3 @@
+(ns advent-2023-clojure.day01
+  (:require [abyala.advent-utils-clojure.core :refer :all]))
+
---
+++ test/advent_2023_clojure/day01_test.clj
@@ -0,0 +1,6 @@
+(ns advent-2023-clojure.day01-test
+  (:require [clojure.test :refer :all]
+            [advent-2023-clojure.day01 :refer :all]))
+
+;(deftest part1-test)
+;(deftest part2-test)
